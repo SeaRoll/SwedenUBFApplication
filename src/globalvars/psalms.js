03653fd49ecbc8fb1b8aa28290c60c68b6355655
jkangho <jkangho@hotmail.com>
2020-08-23 15:28:36 +0200

    ny psalmer 1

--- src/globalvars/psalms.js
+++ src/globalvars/psalms.js
@@ -4,39 +4,55 @@ const psalms = [
         title: "Psalm 15",
         verses: [
             "1. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod.",
-            "2. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod."
+            "2. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod.",
+            "3. Halleluja! Du som bjuder syndare till bords med dig!\nHalleluja! Öppnad famnen är för alla och för mig.\nÖver jordens vidd din förbön genom dag och natt oss når.\nBed för mig inför din Fader, du som vid hans sida står!",
+            "4. Halleluja! Konungarnas konung är du och förblir.\nHalleluja! Född av kvinna är du och du blev som vi.\nStor din härlighet i himlen, speglad över hav och jord;\nstörst när ner bland oss du stiger och oss mättar vid ditt bord."
         ]
     },
     {
         id: 17,
         title: "Psalm 17",
         verses: [
-            "123123123123aaa",
-            "Bruh Im Cold",
-    ]
-    },
-    {
-        id: 15,
-        title: "Psalm 15",
-        verses: [
-            "1. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod.",
-            "2. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod."
+            "1. Ge Jesus äran, frälsta mänsklighet!\nHan är den som segrat i alla evighet!\nHimlen mitt i världen Han oss visat har!\nHan har lämnat graven och är bland oss kvar!\nGe Jesus äran, lovsjung natt och dag:\nHan är segerns Herre, sjung Halleluja!",
+            "2. Vår Herre lever, lovad vare Han!\nEnsam Han åt alla Livets seger vann!\nSalig du som känner: Jesus är vår vän!\nLåt oss därför sjunga om och om igen!\nGe Jesus äran, lovsjung natt och dag:\nHan är segerns Herre, sjung Halleluja!",
+            "3. Honom vi älskar, Han som är vårt stöd,\nHan som hemfört segern över liv och död!\nLåt oss aldrig tvivla på Hans löftes ord!\nHerren, den uppståndne, Himlen på vår jord!\nGe Jesus äran, lovsjung natt och dag:\nHan är segerns Herre, sjung Halleluja!"
         ]
     },
     {
-        id: 15,
-        title: "Psalm 15",
+        id: 21,
+        title: "Psalm 21",
         verses: [
-            "1. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod.",
-            "2. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod."
+            "1. Måne och sol, vatten och vind\noch blommor och barn skapade Gud.\nHimmel och jord, allting är hans.\nHerren vår Gud vill vi tacka.\nHerre, vi tackar dig. Herre, vi prisar dig.\nHerre, vi sjunger ditt heliga namn.",
+            "2. Jesus, Guds Son, levde och dog\nför alla, för oss, lever i dag,\nja, han är här, ja, han är här.\nHerren vår Gud vill vi tacka.\nHerre, vi tackar dig. Herre, vi prisar dig.\nHerre, vi sjunger ditt heliga namn.",
+            "3. Anden, vår tröst, levande, varm\noch helig och stark, talar om Gud\nstöder och bär dag efter dag.\nHerren, vår Gud vill vi tacka.\nHerre, vi tackar dig. Herre, vi prisar dig.\nHerre, vi sjunger ditt heliga namn."
         ]
     },
     {
-        id: 15,
-        title: "Psalm 15",
+        id: 249,
+        title: "Psalm 249",
         verses: [
-            "1. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod.",
-            "2. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod."
+            "1. Blott en dag, ett ögonblick i sänder,\nvilken tröst, vad än som kommer på!\nAllt ju vilar i min Faders händer.\nSkulle jag, som barn, väl ängslas då?\nHan som bär för mig en faders hjärta,\nhan ju ger åt varje nyfödd dag\ndess beskärda del av fröjd och smärta,\nmöda, vila och behag.",
+            "2. Själv han är mig alla dagar nära,\nför var särskild tid med särskild nåd.\nVarje dags bekymmer vill han bära,\nhan som heter både Kraft och Råd.\nMorgondagens omsorg får jag spara,\nom än oviss syns min vandrings stig.\n”Som din dag, så skall din kraft ock vara”,\ndetta löfte gav han mig.",
+            "3. Hjälp mig då att vila tryggt och stilla\nblott vid dina löften, Herre kär,\nej min tro och ej den tröst förspilla\nsom i Ordet mig förvarad är.\nHjälp mig, Herre, att vadhelst mig händer\ntaga ur din trogna fadershand\nblott en dag, ett ögonblick i sänder,\ntills jag nått det goda land."
+        ]
+    },
+    {
+        id: 252,
+        title: "Psalm 252",
+        verses: [
+            "1. Hela vägen går han med mig,\nvad kan jag väl önska mer?\nKan jag tvivla på hans godhet\nnär jag här hans ledning ser?\nHimmelsk frid, gudomlig trygghet\nuti honom har min själ\n//: ty jag vet, vad än mig möter\ngör dock Jesus allting väl ://",
+            "2. Hela vägen går han med mig,\nhjälper mig och är mitt stöd,\nger mig nåd för varje prövning,\nstyrker mig med livets bröd.\nOch om hjärtat skulle törsta,\nvägen kännas tung och lång,\n//: glädjekällor då ur klippan\nspringer fram som förr en gång ://",
+            "3. Hela vägen går han med mig,\n– vilken kärlek hög och rik!\nOch till sist en evig vila\nger han mig i himmelrik.\nNär jag där får fri, förklarad,\ninför honom falla ned\n//: skall med glädje jag det minnas:\nhela vägen gick han med! ://"
+        ]
+    },
+    {
+        id: 261,
+        title: "Psalm 261",
+        verses: [
+            "1. Tack, min Gud, för vad som varit,\ntack för allt vad du beskär!\nTack för tiderna som farit!\nTack för stund, som inne är!\nTack för ljusa, varma vårar,\ntack för mörk och kulen höst!\nTack för redan glömda tårar,\ntack för friden i mitt bröst!",
+            "2. Tack för vad du uppenbarat!\nTack för vad jag ej förstår!\nTack för bön som du besvarat,\ntack för vad jag icke får!\nTack för livets hemligheter!\nTack för hjälp i nödens stund!\nTack för nåd som ingen mäter!\nTack för blodets fridsförbund!",
+            "3. Tack för himmel blå i livet!\nTack för moln du strött därpå!\nTack för solljus, av dig givet!\nTack för mörkret likaså!\nTack för prövningar och strider!\nTack för hopp som uppfyllts väl!\nTack för dagen som framskrider!\nTack för hopp som slagit fel!",
+            "4. Tack för rosorna bland vägen!\nTack för törnet ibland dem!\nTack för resta himlastegen!\nTack för evigt tryggat hem!\nTack för kors och tack för plåga!\nTack för himmelsk salighet!\nTack för stridens klara låga!\nTack för allt i evighet!"   
         ]
     }
 
