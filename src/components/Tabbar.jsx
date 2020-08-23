361c726a9d97335fc3257cfbd0df47d1742df89d
Yohan Joo <nahoyman78@gmail.com>
2020-08-23 13:40:28 +0200

    extract from number to constant.

--- src/components/Tabbar.jsx
+++ src/components/Tabbar.jsx
@@ -9,11 +9,13 @@ import { Link } from "react-router-dom";
 */
 function Tabbar(props) {
 
+    const INCREMENT_VALUE = 5;
+
     return(
     <nav className="navbar fixed-bottom navbar-expand navbar-light bg-light mr-auto">
         <div className="navbar-nav mx-auto">
-            <a className="nav-item nav-link" href="#" onClick={() => props.updateParagraph(5)}>Plus</a>
-            <a className="nav-item nav-link" href="#" onClick={() => props.updateParagraph(-5)}>Minus</a>
+            <a className="nav-item nav-link" href="#" onClick={() => props.updateParagraph(INCREMENT_VALUE)}>Plus</a>
+            <a className="nav-item nav-link" href="#" onClick={() => props.updateParagraph(-INCREMENT_VALUE)}>Minus</a>
             <Link to="/" className="nav-item nav-link">Home</Link>
             <a className="nav-item nav-link" href="/bible">Bible</a>
             <Link to="/psalm" className="nav-item nav-link">Psalm</Link>
