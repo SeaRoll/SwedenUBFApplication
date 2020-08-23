c2f615889c8faafb8941c8c710965832b181e5a8
jkangho <jkangho@hotmail.com>
2020-08-23 13:53:34 +0200

    ny psalm

--- src/globalvars/psalms.js
+++ src/globalvars/psalms.js
@@ -12,7 +12,31 @@ const psalms = [
         title: "Psalm 17",
         verses: [
             "123123123123aaa",
-            "Bruh Im Cold"
+            "Bruh Im Cold",
+    ]
+    },
+    {
+        id: 15,
+        title: "Psalm 15",
+        verses: [
+            "1. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod.",
+            "2. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod."
+        ]
+    },
+    {
+        id: 15,
+        title: "Psalm 15",
+        verses: [
+            "1. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod.",
+            "2. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod."
+        ]
+    },
+    {
+        id: 15,
+        title: "Psalm 15",
+        verses: [
+            "1. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod.",
+            "2. Halleluja! Sjung om Jesus, han är den som spiran bär.\nHalleluja! Hans är makten, hans allena segern är.\nHör, hur sångerna från Sion brusar som en mäktig flod.\nJesus alla folk och stammar återlöste med sitt blod."
         ]
     }
 
